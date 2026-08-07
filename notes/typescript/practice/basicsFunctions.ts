ba8bd101608246849066d2386c63c85a41ed5af5
Venkatesh S <venkatesh230691@gmail.com>
2026-08-07 08:20:50 +0530

    anonymous functions

--- notes/typescript/practice/basicsFunctions.ts
+++ notes/typescript/practice/basicsFunctions.ts
@@ -22,35 +22,92 @@ function addSums(...val: number[]): number {
 function allperforms(...val: number[] | string[]): number | string {
     let summ: number = 0;
     let allData: string = '';
-    for(const valuee of val){
-        if(typeof valuee==='number'){
+    for (const valuee of val) {
+        if (typeof valuee === 'number') {
             summ += valuee;
-        }else{
+        } else {
             allData = valuee.toUpperCase();
         }
     }
     return summ || allData;
 }
 
-const resultFinal=allperforms(1, 2, 3, 4, 5);
-const res=allperforms("hello , world");
+const resultFinal = allperforms(1, 2, 3, 4, 5);
+const res = allperforms("hello , world");
 console.log(`Final Result : ${resultFinal}`);
 console.log(`String Result : ${res}`);
 
-function getAllData(a:number, b:string, c?:boolean):void{
-    if(c){
-       console.log(`a : ${a} b : ${b} c : ${c}`); 
-    }else{
+function getAllData(a: number, b: string, c?: boolean): void {
+    if (c) {
+        console.log(`a : ${a} b : ${b} c : ${c}`);
+    } else {
         console.log(`a : ${a} b : ${b}`)
     }
 }
 
-getAllData(10,"Kaushik",true);
+getAllData(10, "Kaushik", true);
 
-function defaultDiscount(value : number,div:number=2){
-    let ans=value/div;
+function defaultDiscount(value: number, div: number = 2) {
+    let ans = value / div;
     return ans;
 }
 
-const ansans=defaultDiscount(3);
+const ansans = defaultDiscount(3);
 console.log(`The answer : ${ansans}`);
+
+
+/* anonymous functions */
+
+let messages = function (): string {
+    return "Kaushik how are you";
+}
+
+console.log(messages());
+
+let allSums = function (x: number, y: number): number {
+    return x + y;
+}
+
+console.log(allSums(2, 3));
+
+let calcu = function (...vals: number[]): number {
+    let sum = 0;
+    for (const val of vals) {
+        sum += val;
+    }
+    return sum;
+}
+console.log(calcu(1, 2, 3))
+
+let checks = function (...val: number[] | string[]): number | string {
+    let total: number = 0;
+    let upperCase: string = "";
+    for (let v of val) {
+        if (typeof v === 'number') {
+            total += v;
+        } else {
+            upperCase = v.toUpperCase();
+        }
+    }
+    return total || upperCase;
+}
+
+console.log(checks("god is great"))
+console.log(checks(10, 20, 30))
+
+let checking = function (...datas: number[] | string[]): number | string {
+    let calcDatas: number = 0;
+    let cases: string = "";
+    for (const data of datas) {
+        (typeof data === 'number') ? (calcDatas += data) : (cases = data.toUpperCase());
+    }
+    return calcDatas || cases;
+}
+
+console.log(checking("kadavule kapathu"));
+console.log(checking(1, 2, 3, 4, 5));
+
+let opt = function (names: string, age: number, emails?: string) {
+    return emails ? `name : ${names}, age : ${age}, email : ${emails}` : `name : ${names}, age : ${age}`
+}
+console.log(opt("Babbi",35,"abc@mail.com"));
